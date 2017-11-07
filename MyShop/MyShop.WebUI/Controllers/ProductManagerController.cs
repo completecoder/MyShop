a799c2982501fd0c286693d6985ac23ba96a6022
Brett Hargreaves <bretthargreaves@hotmail.com>
2017-11-07 14:05:05 +0000

    added missing commit

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -115,6 +115,7 @@ namespace MyShop.WebUI.Controllers
             else
             {
                 context.Delete(Id);
+                context.Commit();
                 return RedirectToAction("Index");
             }
         }
