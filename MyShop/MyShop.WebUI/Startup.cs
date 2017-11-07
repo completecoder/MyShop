37fa0bcaf84bf3a39ca44b13f159dc543ef25a46
Brett Hargreaves <bretthargreaves@hotmail.com>
2017-11-07 09:31:05 +0000

    Initial Solution Setup

---
+++ MyShop/MyShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.WebUI.Startup))]
+namespace MyShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
