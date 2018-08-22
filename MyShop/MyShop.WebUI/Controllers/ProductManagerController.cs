7a31d7cc589000af38896e872ed5af3a9cd9b94e
Mahesh Sathavalli <mahesh.malli07@gmail.com>
2018-08-22 15:01:46 +0530

    Testathon

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -11,7 +11,7 @@ using System.IO;
 
 namespace MyShop.WebUI.Controllers
 {
-    [Authorize(Roles = "Admin")]
+   
     public class ProductManagerController : Controller
     {
         IRepository<Product> context;
